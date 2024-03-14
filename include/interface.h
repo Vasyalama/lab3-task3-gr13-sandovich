cdc6f7f98598e1aa1bab49bca919fb763a5e7d46
sandovich <sanpinchy@gmail.com>
2024-03-14 16:04:32 +0300

    added include

---
+++ include/interface.h
@@ -0,0 +1,18 @@
+#ifndef LAB3_3_INTERFACE_H
+#define LAB3_3_INTERFACE_H
+
+typedef struct{
+  int number;
+  char* start;
+  char* end;
+  int length;
+} MARSHRUT;
+
+MARSHRUT* createArrayOfMarshrut(int amount);
+MARSHRUT getLongestMarshrut(MARSHRUT* arr, int arrLen);
+int compare(const void* a, const void* b);
+void sortByNumber(MARSHRUT* arr, int size);
+void printMarshrut(MARSHRUT m);
+void printStartAndEndInSamePlace(MARSHRUT* arr, int size, char* place);
+
+#endif
